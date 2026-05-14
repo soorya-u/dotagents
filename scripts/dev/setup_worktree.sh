8b9be4dbf30400c2e73b6957c148a41a56c08cf2
Soorya U <sooryau7@gmail.com>
2026-05-14 15:23:30 +0530

    touch local config before deploy in setup_worktree
    - Prevents dotagents deploy from failing when local.config.toml is missing

--- scripts/dev/setup_worktree.sh
+++ scripts/dev/setup_worktree.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 mise trust
+touch .dotagents/local.config.toml # a hack to make dotagents run. will be fixed in next update
 mise exec -- dotagents deploy < /dev/null
 bun i --cwd tests/e2e
